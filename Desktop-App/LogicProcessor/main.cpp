8a843ea37a90c9f9fd5ff2d4972315397efcf8a6
Sandesh J. Ghimire <sandeshghimire@outlook.com>
2016-09-08 09:46:14 -0700

    Update new

--- Desktop-App/LogicProcessor/main.cpp
+++ Desktop-App/LogicProcessor/main.cpp
@@ -5,6 +5,7 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+    w.setWindowTitle("Processor Logic Configuraiton Tools ");
     w.show();
 
     return a.exec();
